5b69375f0d84dcf4a51251b80c7a86aa860aba99
kusnii <mkhusni377@gmail.com>
2025-03-08 11:21:43 +0700

    menampilkan data dari database

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,17 +2,23 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    @var array
-    
-protected $fillable = [
+    use HasFactory;
+
+    /**
+     * fillable
+     *
+     * @var array
+     */
+    protected $fillable = [
         'image',
         'title',
         'description',
         'price',
-        'stock'
+        'stock',
     ];
 }
